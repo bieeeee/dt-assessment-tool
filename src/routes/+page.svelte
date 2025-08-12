621280354a3ed088bb246887e81172d33db88cec
bieeeee <janviertrois@naver.com>
2025-08-12 15:11:59 +0900

    feat: add initial routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { goto } from "$app/navigation";
+</script>
+
+<div>
+  <h1 class="text-3xl font-bold">
+    Assess Your Digital Transformation Readiness
+  </h1>
+  <button onclick={() => goto("/assessment")}>Start</button>
+</div>
+
+<style lang="postcss">
+  @reference "tailwindcss";
+  :global(html) {
+    background-color: theme(--color-gray-100);
+  }
+</style>
---
+++ src/routes/assessment/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<div>
+  <h1>Tell us about your company</h1>
+</div>
+
+<style>
+</style>
